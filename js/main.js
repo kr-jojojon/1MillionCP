9d94b16f7b738072326717ba9e5f3dd414a4758c
Tatsuran <tatsuran@gmail.com>
2021-10-31 19:19:34 +0000

    Fan art implementation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-	$(function(){
-		$('a[href^="#"]').click(function(){
+$(function(){
+	$('a[href^="#"]').click(function(){
 		var adjust = 0;
 		var speed = 800;
 		var href= $(this).attr("href");
@@ -7,20 +7,55 @@
 		var position = target.offset().top + adjust;
 		$('body,html').animate({scrollTop:position}, speed, 'swing');
 		return false;
-	});
-	});window.onload=function(){
-	const menuBtn = document.querySelector('.menu-btn');
-	const headerBox = document.querySelector('#header-box');
-	let menuOpen = false;
-	menuBtn.addEventListener('click', () => {
-		if(!menuOpen) {
-			menuBtn.classList.add('open');
-			headerBox.classList.add('open');
-			menuOpen = true;
-		} else {
-			menuBtn.classList.remove('open');
-			headerBox.classList.remove('open');
-			menuOpen = false;
+		});
+
+		const menuBtn = document.querySelector('.menu-btn');
+		const headercontents_box = document.querySelector('#header-contents_box');
+		let menuOpen = false;
+		menuBtn.addEventListener('click', () => {
+			if(!menuOpen) {
+				menuBtn.classList.add('open');
+				headercontents_box.classList.add('open');
+				menuOpen = true;
+			} else {
+				menuBtn.classList.remove('open');
+				headercontents_box.classList.remove('open');
+				menuOpen = false;
+			}
+		});
+
+		var folder = "img/fanart/";
+
+		$.ajax({url: folder, success: function (data) {
+			$(data).find("a").attr("href", function (i, val) {
+				if( val.match(/\.(jpe?g|png|gif)$/) ) {
+					name = val.split('_').slice(1).join('_').split('.')[0].replace('%20', ' ')
+
+					contents_warp = document.getElementsByClassName("contents-warp")[0];
+					contents_box = document.createElement("div");
+					contents_box.classList.add("contents-box");
+					fa_frame = document.createElement("div");
+					fa_frame.classList.add("fa-frame");
+					contents_name = document.createElement("p");
+					text = document.createTextNode(name);
+					contents_name.classList.add("contents-name");
+					anchor = document.createElement("a");
+					anchor.setAttribute("href", folder + val);
+					anchor.setAttribute("data-lightbox", "image-1");
+					anchor.setAttribute("data-title", name);
+					anchor.setAttribute("data-alt", "fanart sent by " + name);
+					image = document.createElement("img");
+					image.setAttribute("src", folder + val);
+					image.setAttribute("alt", name);
+
+					contents_warp.appendChild(contents_box);
+					contents_box.appendChild(fa_frame);
+					contents_box.appendChild(contents_name);
+					fa_frame.appendChild(anchor);
+					contents_name.appendChild(text);
+					anchor.appendChild(image);
+				};
+			});
 		}
-	})
-}
+	});
+});
