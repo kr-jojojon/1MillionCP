39c3704264b5632232e685464428fcf53281fd2b
Tatsuran <tatsuran@gmail.com>
2021-11-02 06:45:24 +0000

    Displaying actual names instead of twitter handles

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const FANART = `
-sss_mspink_y.jpg
-sss_TE___jin.jpg
-sss_aobushi.jpg
-2i_piyokinoko.png
-1i_piyokinoko.jpg
-C4_Tatsuroti.png
-2b_BlueOverflag.jpg
-sss_Tempura Person.jpg
-2k_tokitosonem.jpg
-1s_LeslieEmpty.png
-1s_LeslieEmpty.jpg
-2s_Soab.png
-2s_ykmn_illust.jpg
-2a_ArKhey.jpg
-`.split('\n')
-
-
 $(function(){
 	$('a[href^="#"]').click(function(){
 		var adjust = 0;
@@ -27,8 +9,8 @@ $(function(){
 		return false;
 		});
 
-	const menuBtn = document.querySelector('.menu-btn');
-	const headerBox = document.querySelector('#header-box');
+	let menuBtn = document.querySelector('.menu-btn');
+	let headerBox = document.querySelector('#header-box');
 	let menuOpen = false;
 	menuBtn.addEventListener('click', () => {
 		if(!menuOpen) {
@@ -42,25 +24,27 @@ $(function(){
 		}
 	});
 
-	for (let i = 0; i < FANART.length; i++) {
-		if (FANART[i] != ""){
-	  		name = FANART[i].split('_').slice(1).join('_').split('.')[0].replace('%20', ' ')
-	  		url = "fanart/" + FANART[i]
-			console.log("name: " + name + "url: " + url)
-			contentsWarp = document.getElementsByClassName("contents-warp")[0];
-			contentsBox = document.createElement("div");
+	$.getJSON("data/display_names.json", function(displayNames){
+		for (filename in displayNames) {
+	  		let name = displayNames[filename]
+	  		// let handle = FANART[i].split('_').slice(1).join('_').split('.')[0].replace('%20', ' ')
+
+	  		let url = "fanart/" + filename
+
+			let contentsWarp = document.getElementsByClassName("contents-warp")[0];
+			let contentsBox = document.createElement("div");
 			contentsBox.classList.add("contents-box");
-			faFrame = document.createElement("div");
+			let faFrame = document.createElement("div");
 			faFrame.classList.add("fa-frame");
-			contentsName = document.createElement("p");
-			text = document.createTextNode(name);
+			let contentsName = document.createElement("p");
+			let text = document.createTextNode(name);
 			contentsName.classList.add("contents-name");
-			anchor = document.createElement("a");
+			let anchor = document.createElement("a");
 			anchor.setAttribute("href", url);
 			anchor.setAttribute("data-lightbox", "image-1");
 			anchor.setAttribute("data-title", name);
 			anchor.setAttribute("data-alt", "fanart sent by " + name);
-			image = document.createElement("img");
+			let image = document.createElement("img");
 			image.setAttribute("src", url);
 			image.setAttribute("alt", name);
 
@@ -70,6 +54,6 @@ $(function(){
 			faFrame.appendChild(anchor);
 			contentsName.appendChild(text);
 			anchor.appendChild(image);
-	  	}
-	}
+		}
+	});
 });
